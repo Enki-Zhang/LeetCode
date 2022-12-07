3e20afd2473a9a184cf3ade4c8676691a6454a37
Enki-Zhang <17855378518@163.com>
2022-12-07 12:28:14 +0800

    2022-12-7

--- src/leetCodeBack/LeetCode_78.java
+++ src/leetCodeBack/LeetCode_78.java
@@ -7,25 +7,29 @@ import java.util.List;
  * @author Enki
  * @Version 1.0
  */
-public class LeetCode_77 {
+public class LeetCode_78 {
     public static void main(String[] args) {
+        int[] nums = {1};
+        List<List<Integer>> subsets = new Solution().subsets(nums);
+        System.out.println(subsets);
 
     }
-//    组合问题
-    class Solution {
-        List<List<Integer>> lists = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
 
-        public List<List<Integer>> combine(int n, int k) {
-            int[] nums = new int[n];
-            for (int i = 0; i < n; i++) {
-                nums[i] = i + 1;
+    //    给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+    // 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+    static class Solution {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        public List<List<Integer>> subsets(int[] nums) {
+
+            for (int i = 0; i <= nums.length; i++) {
+                backTrack(nums, i, 0, new ArrayList<Integer>());
             }
-            backTrack(nums, k, 0);
+
             return lists;
         }
 
-        public void backTrack(int[] nums, int k, int start) {
+        public void backTrack(int[] nums, int k, int start, ArrayList<Integer> list) {
             if (list.size() == k) {
                 ArrayList<Integer> integers = new ArrayList<>(list);
                 lists.add(integers);
@@ -36,10 +40,9 @@ public class LeetCode_77 {
 //            nums.length - (k - list.size())+1 元素遍历的起始点 因为是顺序加入节点的
             for (int i = start; i < nums.length - (k - list.size()) + 1; i++) {
                 list.add(nums[i]);
-                backTrack(nums, k, i + 1);
+                backTrack(nums, k, i + 1, list);
                 list.remove(list.size() - 1);
             }
         }
     }
-
 }
