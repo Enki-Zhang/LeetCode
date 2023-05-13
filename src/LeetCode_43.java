3912bafe43666e4050f0e3f85926bc5fa16cc4b1
Enki-Zhang <17855378518@163.com>
2023-05-13 11:05:58 +0800

    2023-5-13

---
+++ src/LeetCode_43.java
@@ -0,0 +1,14 @@
+/**
+ * @author Enki
+ * @Version 1.0
+ */
+public class LeetCode_43 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public String multiply(String num1, String num2) {
+
+        }
+    }
+}
