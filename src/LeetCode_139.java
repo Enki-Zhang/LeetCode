a2f4d2356d46759d1bdeea0cbbd5e60b7247ecee
Enki-Zhang <17855378518@163.com>
2023-07-27 11:47:46 +0800

    LeetCode

---
+++ src/LeetCode_139.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * @author Enki
+ * @Version 1.0
+ */
+public class LeetCode_139 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 单词拆分 匹配 前缀树
+     */
+    class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+
+            return false;
+
+        }
+    }
+}
--- src/LeetCode_208.java
+++ src/LeetCode_208.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import tree.TreeNode;
 
 /**
@@ -6,89 +7,95 @@ import tree.TreeNode;
  */
 public class LeetCode_208 {
     public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        boolean apple = trie.search("apple");
+        System.out.println(apple);
 
     }
 
     /**
-     *  前缀树
+     * 前缀树
      */
-    class Trie {
-//        使用数组进行模拟
-        private Trie[] children;
-        private boolean isEnd;
+    static class Trie {
+        //        使用数组进行模拟
+        private Trie[] children; //孩子节点 用于查找
+        private boolean isEnd; //判断是到达单词结尾处
 
         /**
-         * 初始化前缀树 单词全是小写英文字母构成 前缀树的子节点有26个 是个多叉树
+         * 初始化前缀 树 单词全是英文字母构成 前缀树的子节点有26个是个多叉树
          */
         public Trie() {
 //           父节点的子节点 字母位 26
             children = new Trie[26];
-            isEnd = false;
+//            isEnd = false;
         }
 
         /**
          * 向树中插入Word 如果存在子节点 寻找子节点字符位是否存在 存在则不需要操作搜索下一个字符 不存在存入字符
+         *
          * @param word
          */
         public void insert(String word) {
-//            前缀树赋值
+//            前缀树赋值  node用来遍历
             Trie trie = this;
-            for (int i =0;i<word.length();i++) {
+            //搜索每一个字符
+            for (int i = 0; i < word.length(); i++) {
                 char a = word.charAt(i);
-                int index = a- 'a';//index为字符位
-                if (trie.children[index]==null) {
+                int index = a - 'a';//index为字符位
+                if (trie.children[index] == null) {
 //                    插入 构建子节点
                     trie.children[index] = new Trie();
-                }else {
-                    trie = trie.children[index];
                 }
+//                   无论有没有都需要 指针后移 纵向搜索创建
+                trie = trie.children[index];
+
             }
+            trie.isEnd = true;
         }
 
         /**
          * 节点搜索 从根节点搜索前缀
+         *
          * @param word
          * @return
          */
         public boolean search(String word) {
             Trie trie = this;
-            boolean end = false;
             for (int i = 0; i < word.length(); i++) {
                 char a = word.charAt(i);
-                int index = a-'a';
-                if (trie.children[index]==null) {
-                    end = trie.children[index].isEnd;
+                int index = a - 'a';
+                if (trie.children[index] == null) {
                     return false;
-                }else {
-                    end = trie.children[index].isEnd;
-                    trie = trie.children[index];
                 }
+                trie = trie.children[index];
+
             }
-            return end = true;
+            return trie.isEnd;
 
         }
 
         /**
          * 搜索前缀 一直搜到了结尾 为true
+         *
          * @param prefix
          * @return
          */
         public boolean startsWith(String prefix) {
-            Trie trie = new Trie();
-            boolean end = false;
+            Trie trie = this;
+//            boolean end = false;
             for (int i = 0; i < prefix.length(); i++) {
                 char a = prefix.charAt(i);
-                int index = a-'a';
-
-                if (trie.children[index]==null) {
-                    end = trie.children[index].isEnd;
+                int index = a - 'a';
+                if (trie.children[index] == null) {
+//                    end = trie.children[index].isEnd;
                     return false;
-                }else {
-                    end = trie.children[index].isEnd;
-                    trie = trie.children[index];
                 }
+//                    end = trie.children[index].isEnd;
+                trie = trie.children[index];
+
             }
-            return end;
+            return true;
         }
     }
 }
