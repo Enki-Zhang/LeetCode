a194c7fd88b41f100a0ca9fecece2d8f422bd178
Enki-Zhang <17855378518@163.com>
2023-08-17 14:51:11 +0800

    LeetCode

--- src/LeetCode_376.java
+++ src/LeetCode_376.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * @author Enki
  * @ClassName LeetCode_376
@@ -8,27 +11,41 @@
 public class LeetCode_376 {
     public static void main(String[] args) {
 //        TODO
-
+        Solution solution = new Solution();
+        System.out.println(solution.wiggleMaxLength(new int[]{1,0,1}));
     }
 
     /**
-     * 摆动数列 遍历数组记录满足上升下降的数字组合
+     * 摆动数列 求子序列最长 假设每一个数字追加后都满足摆动特性即可
+     * 或者记录两个数字上升或者下降
      */
-    class Solution {
+    static class Solution {
         public int wiggleMaxLength(int[] nums) {
-            boolean isRise = false;
-            int pre = nums[0];
-            for (int i = 1; i < nums.length; i++) {
-                pre = nums[i - 1];
-                if (nums[i] > pre) {
-                    isRise = true;
-                }
-                if (nums[i] == pre) {
-                    continue;
-                }
-
+            if (nums.length == 1) {
+                return 1;
             }
-return nums.length;
+            int changes = 0;
+            int dir = -1;
+
+            for (int i = 1; i < nums.length; i++) {
+//
+                if (nums[i] == nums[i - 1]) continue;
+                if (nums[i] > nums[i - 1]) {
+                    if (dir == 1) {
+                        continue;
+                    }
+                    dir = 1;
+                    changes++;
+                }
+                if (nums[i] < nums[i - 1]) {
+                    if (dir == 0) {
+                        continue;
+                    }
+                    dir = 0;
+                    changes++;
+                }
+            }
+            return changes + 1;
         }
     }
 }
--- src/LeetCode_53.java
+++ src/LeetCode_53.java
@@ -1,3 +1,4 @@
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
@@ -7,7 +8,7 @@ import java.util.Arrays;
 public class LeetCode_53 {
     public static void main(String[] args) {
         Solution2 solution2 = new Solution2();
-        int[] ints = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] ints = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int i = solution2.maxSubArray(ints);
         System.out.println(i);
     }
@@ -19,12 +20,12 @@ public class LeetCode_53 {
     static class Solution {
         public int maxSubArray(int[] nums) {
             int left = 0;
-            int sum =0;
+            int sum = 0;
             int max = Integer.MIN_VALUE;
             for (int num :
                     nums) {
                 sum += num;
-                max = Math.max(sum,max);
+                max = Math.max(sum, max);
 
             }
 
@@ -36,19 +37,39 @@ public class LeetCode_53 {
     /**
      * 动态规划 每个小连续数组新加元素后是否变大 是则新增 否 则弃掉之前的数组 从新选择
      */
-   static class Solution2 {
+    static class Solution2 {
 
         public int maxSubArray(int[] nums) {
             int max = Integer.MIN_VALUE;
-            int dp[] =new int[nums.length];
+            int dp[] = new int[nums.length];
 //            初始化dp
             dp[0] = nums[0];
 //            dp[i] = dp[i-1] + num;
             for (int i = 1; i < nums.length; i++) {
-                dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
+                dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             }
             Arrays.sort(dp);
-            return dp[dp.length-1];
+            return dp[dp.length - 1];
+        }
+    }
+
+    /**
+     * 贪心 这题不需要返回子数组 那么选择累加元素的数组起点 将后面的数组进行累加 当累加出现负数则更新累加数组的起点为当前下一个元素 并不断更新最大值
+     */
+    class Solution3 {
+        public int maxSubArray(int[] nums) {
+
+            int max = Integer.MIN_VALUE;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                count += nums[i];
+                max = Math.max(count, max);
+                if (count < 0) {
+                    count = 0;
+                }
+            }
+            return max;
+
         }
     }
 
