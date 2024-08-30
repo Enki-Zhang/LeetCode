cee3fef6dfd8a70ce26c24c8f0f8b88b8aad454c
Enki-Zhang <17855378518@163.com>
2024-08-30 08:21:05 +0800

    2024-8-29

--- leetcode02/src/algorithm/LeetCode_27.java
+++ leetcode02/src/algorithm/LeetCode_27.java
@@ -9,11 +9,26 @@ package algorithm;
  */
 public class LeetCode_27 {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] ints = {0, 1, 2, 2, 3, 0, 4, 2};
+        int i = solution.removeElement(ints, 2);
+        System.out.println(i);
     }
-    class Solution {
+
+    static class Solution {
         public int removeElement(int[] nums, int val) {
-            return 0;
+            int len = 0;
+
+//                题目不需要进行交换 只要在新数组中不保留目标值即可 用后面数字替换前面的目标值即可
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) {
+                    nums[len] = nums[i];
+                    len++;
+                }
+
+            }
+            return len;
         }
+
     }
 }
